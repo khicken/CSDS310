cb1a597f43fad8d912ed41e2de7be47c3c35aa6a
kaleb <kalebkim1939@gmail.com>
2024-11-03 15:38:28 -0500

    Update main.py

--- Presentation/main.py
+++ Presentation/main.py
@@ -14,28 +14,29 @@ def starmaxxing(S) -> int:
         print(f'lvl: {lvl} | c: {c} | A: {str(A)}')
     return c
 
-def starmaxxing2(pixels):
-    n = len(pixels)
-    prefix_sum = 0
-    star_dense_count = 0
-    prefix_sum_count = {0: 1}  # Offset to handle cases where prefix_sum is already positive
-
-    for pixel in pixels:
-        # Update prefix_sum based on whether the pixel is a star (1) or empty space (0)
-        prefix_sum += 1 if pixel == 1 else -1
-        
-        # Count how many previous prefix sums are less than the current one
-        # All subarrays ending here that would have net stars > empty spaces
-        for prev_prefix_sum in prefix_sum_count:
-            if prev_prefix_sum < prefix_sum:
-                star_dense_count += prefix_sum_count[prev_prefix_sum]
-            print(f'hello {star_dense_count}')
-
-        # Record the current prefix_sum in the map
-        prefix_sum_count[prefix_sum] = prefix_sum_count.get(prefix_sum, 0) + 1
-
-    return star_dense_count  # Output should be 9
-
-print(starmaxxing2([0,1,1,0,1,1]))
-# print(starmaxxing2([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]))
-# print(starmaxxing2([1,1,1,1]))
+def starmaxxing2(S):
+    n = len(S)
+    cs = 0
+    pc_e0 = 0
+    pc_g0 = 0
+    sf = [0] * (2*n+1)
+    sf[n] = 1
+    c = 0
+    for s in S:
+        if s:
+            cs += 1
+            cc_g0 = 1 + pc_e0 + pc_g0
+        else:
+            cs -= 1
+            cc_g0 = pc_g0 - sf[n+cs]
+        pc_g0 = cc_g0
+        pc_e0 = sf[n+cs]
+        sf[n+cs] += 1
+        c += cc_g0
+    return c
+    
+tests = [[0,1,1,0,1,1], [0,1,1,0,1], [1,1,1,1,0]]
+for t in tests:
+    print(f'for {t}:')
+    print(starmaxxing(t))
+    print(starmaxxing2(t))
